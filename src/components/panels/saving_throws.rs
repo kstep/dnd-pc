587d69cba6ea11e1159b87bba8430915cca18ea8
Konstantin Stepanov <milezv@gmail.com>
2026-04-12 10:15:42 +0300

    feat: collapsible damage modifiers section in saving throws panel

--- src/components/panels/saving_throws.rs
+++ src/components/panels/saving_throws.rs
@@ -35,6 +35,7 @@ pub fn SavingThrowsPanel() -> impl IntoView {
     let store = expect_context::<Store<Character>>();
 
     let i18n = expect_context::<leptos_fluent::I18n>();
+    let dmg_expanded = RwSignal::new(false);
 
     view! {
         <Panel title=move_tr!("panel-saving-throws") class="saving-throws-panel">
@@ -70,79 +71,99 @@ pub fn SavingThrowsPanel() -> impl IntoView {
                 .collect_view()}
 
             // --- Damage Modifiers ---
-            <h4 class="panel-subsection-title">{move_tr!("session-damage-modifiers")}</h4>
-            {DamageType::iter()
-                .map(|damage_type| {
-                    let current = Memo::new(move |_| {
-                        store
-                            .damage_modifiers()
-                            .read()
-                            .get(&damage_type)
-                            .copied()
-                            .unwrap_or_default()
-                    });
-                    let tr_key = damage_type.tr_key();
-                    let label = Signal::derive(move || i18n.tr(tr_key));
-                    let icon = damage_type.icon_name();
-
-                    let toggle_field = move |field: fn(&mut DamageModifiers) -> &mut bool| {
-                        store.damage_modifiers().update(|damage_modifiers| {
-                            let entry = damage_modifiers.entry(damage_type).or_default();
-                            let flag = field(entry);
-                            *flag = !*flag;
-                            if !entry.is_active() {
-                                damage_modifiers.remove(&damage_type);
-                            }
+            <div class="section-header">
+                <button
+                    class="btn-toggle-desc"
+                    class:expanded=move || dmg_expanded.get()
+                    on:click=move |_| dmg_expanded.update(|expanded| *expanded = !*expanded)
+                />
+                <h4 class="panel-subsection-title">{move_tr!("session-damage-modifiers")}</h4>
+            </div>
+            {move || {
+                let expanded = dmg_expanded.get();
+                DamageType::iter()
+                    .filter(move |damage_type| {
+                        expanded
+                            || store
+                                .damage_modifiers()
+                                .read()
+                                .get(damage_type)
+                                .copied()
+                                .unwrap_or_default()
+                                .is_active()
+                    })
+                    .map(|damage_type| {
+                        let current = Memo::new(move |_| {
+                            store
+                                .damage_modifiers()
+                                .read()
+                                .get(&damage_type)
+                                .copied()
+                                .unwrap_or_default()
                         });
-                    };
+                        let tr_key = damage_type.tr_key();
+                        let label = Signal::derive(move || i18n.tr(tr_key));
+                        let icon = damage_type.icon_name();
 
-                    view! {
-                        <div class="damage-row">
-                            <span class="damage-dt-icon"><Icon name=icon size=14 /></span>
-                            <span class="damage-label">{label}</span>
-                            <DamageToggle
-                                icon="shield-half"
-                                title=Signal::derive(move || i18n.tr("damage-resistance"))
-                                active=Memo::new(move |_| current.get().resistant)
-                                on_toggle=move || toggle_field(|modifiers| &mut modifiers.resistant)
-                            />
-                            <DamageToggle
-                                icon="shield-off"
-                                title=Signal::derive(move || i18n.tr("damage-vulnerability"))
-                                active=Memo::new(move |_| current.get().vulnerable)
-                                on_toggle=move || toggle_field(|modifiers| &mut modifiers.vulnerable)
-                            />
-                            <DamageToggle
-                                icon="shield-check"
-                                title=Signal::derive(move || i18n.tr("damage-immunity"))
-                                active=Memo::new(move |_| current.get().immune)
-                                on_toggle=move || toggle_field(|modifiers| &mut modifiers.immune)
-                            />
-                            <span class="damage-dr">
-                                <Icon name="shield-minus" size=14 />
-                                <input
-                                    type="number"
-                                    min="0"
-                                    class="short-input"
-                                    prop:value=move || current.get().reduction
-                                    on:input=move |event| {
-                                        let value = event_target_value(&event)
-                                            .parse::<u32>()
-                                            .unwrap_or(0);
-                                        store.damage_modifiers().update(|damage_modifiers| {
-                                            let entry = damage_modifiers.entry(damage_type).or_default();
-                                            entry.reduction = value;
-                                            if !entry.is_active() {
-                                                damage_modifiers.remove(&damage_type);
-                                            }
-                                        });
-                                    }
+                        let toggle_field = move |field: fn(&mut DamageModifiers) -> &mut bool| {
+                            store.damage_modifiers().update(|damage_modifiers| {
+                                let entry = damage_modifiers.entry(damage_type).or_default();
+                                let flag = field(entry);
+                                *flag = !*flag;
+                                if !entry.is_active() {
+                                    damage_modifiers.remove(&damage_type);
+                                }
+                            });
+                        };
+
+                        view! {
+                            <div class="damage-row">
+                                <span class="damage-dt-icon"><Icon name=icon size=14 /></span>
+                                <span class="damage-label">{label}</span>
+                                <DamageToggle
+                                    icon="shield-half"
+                                    title=Signal::derive(move || i18n.tr("damage-resistance"))
+                                    active=Memo::new(move |_| current.get().resistant)
+                                    on_toggle=move || toggle_field(|modifiers| &mut modifiers.resistant)
                                 />
-                            </span>
-                        </div>
-                    }
-                })
-                .collect_view()}
+                                <DamageToggle
+                                    icon="shield-off"
+                                    title=Signal::derive(move || i18n.tr("damage-vulnerability"))
+                                    active=Memo::new(move |_| current.get().vulnerable)
+                                    on_toggle=move || toggle_field(|modifiers| &mut modifiers.vulnerable)
+                                />
+                                <DamageToggle
+                                    icon="shield-check"
+                                    title=Signal::derive(move || i18n.tr("damage-immunity"))
+                                    active=Memo::new(move |_| current.get().immune)
+                                    on_toggle=move || toggle_field(|modifiers| &mut modifiers.immune)
+                                />
+                                <span class="damage-dr">
+                                    <Icon name="shield-minus" size=14 />
+                                    <input
+                                        type="number"
+                                        min="0"
+                                        class="short-input"
+                                        prop:value=move || current.get().reduction
+                                        on:input=move |event| {
+                                            let value = event_target_value(&event)
+                                                .parse::<u32>()
+                                                .unwrap_or(0);
+                                            store.damage_modifiers().update(|damage_modifiers| {
+                                                let entry = damage_modifiers.entry(damage_type).or_default();
+                                                entry.reduction = value;
+                                                if !entry.is_active() {
+                                                    damage_modifiers.remove(&damage_type);
+                                                }
+                                            });
+                                        }
+                                    />
+                                </span>
+                            </div>
+                        }
+                    })
+                    .collect_view()
+            }}
         </Panel>
     }
 }
